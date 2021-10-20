3e4680198c2f47ff5f2d15f1b1acc93e495c053c
mahambetberikov <63587147+mahambetberikov@users.noreply.github.com>
2021-10-21 00:17:57 +0600

    commit from Makhambet
    commit from Makhambet

---
+++ home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Home extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Home', style: TextStyle(color: Colors.white),),
+    );
+  }
+}
