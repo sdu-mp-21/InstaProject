8d2d42904a4695a2944c7306f5d53e847f79e73f
Rakhat <crazyrakhat@gmail.com>
2021-10-21 02:38:56 +0600

    Commit from Rakhat

---
+++ market.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Market extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Market', style: TextStyle(color: Colors.white),),
+    );
+  }
+}
