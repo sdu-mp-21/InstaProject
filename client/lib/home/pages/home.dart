c08a90ea19317ec3a7bbfe0dd61a58ab706972d7
rakhatlesbek <92881183+rakhatlesbek@users.noreply.github.com>
2021-11-18 15:51:02 +0600

    Update home.dart

--- client/lib/home/pages/home.dart
+++ client/lib/home/pages/home.dart
@@ -5,7 +5,8 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
-import 'package:instagram/home/pages/profile.dart';
+import 'direct.dart';
+//import 'package:instagram/home/pages/profile.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 class Result {
   // final int userId;
@@ -33,7 +34,8 @@ class Home extends StatelessWidget {
 
     final response = await http.get(
       Uri.parse(
-          'http://localhost:5000/search?token=${sharedPrefs.getString('token')}&q=$query'),
+          'http://localhost:5000/search?token=${sharedPrefs.getString(
+              'token')}&q=$query'),
     );
 
     if (response.statusCode == 200) {
@@ -42,38 +44,42 @@ class Home extends StatelessWidget {
       throw Exception('Failed to load album');
     }
   }
+
   final storyProfile = Expanded(
     child: ListView.builder(
       scrollDirection: Axis.horizontal,
 
-      itemBuilder: (context,index)=>Stack(
-        alignment: Alignment.topLeft,
-        children: [
-          Container(
-            width: 60.0,
-            height: 60.0,
-            decoration:  const BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(image:NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1200px-Circle-icons-profile.svg.jpg'),
-                fit: BoxFit.fill,
-                ),
+      itemBuilder: (context, index) =>
+          Stack(
+            alignment: Alignment.topLeft,
+            children: [
+              Container(
+                width: 60.0,
+                height: 60.0,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(image: NetworkImage(
+                      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1200px-Circle-icons-profile.svg.jpg'),
+                    fit: BoxFit.fill,
+                  ),
 
-            ),
-            margin: const EdgeInsets.symmetric(horizontal: 8.0),
+                ),
+                margin: const EdgeInsets.symmetric(horizontal: 8.0),
+              ),
+              index == 0 ? const Positioned(
+                right: 10.0,
+                child: CircleAvatar(
+                  backgroundColor: Colors.blueAccent,
+                  radius: 10.0,
+                  child: Icon(Icons.add, size: 14.0,
+                    color: Colors.white,),
+                ),
+              ) : Container()
+            ],
           ),
-          index == 0 ? const Positioned(
-            right: 10.0,
-            child: CircleAvatar(
-              backgroundColor: Colors.blueAccent,
-              radius: 10.0,
-              child: Icon(Icons.add,size: 14.0,
-                color: Colors.white,),
-            ),
-          ): Container()
-        ],
-      ),
     ),
   );
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -82,13 +88,20 @@ class Home extends StatelessWidget {
         centerTitle: true,
         leading: Icon(Icons.camera_alt),
         title: const Text("Instagram"),
-        actions: const <Widget>[
-          Padding(
-            padding: EdgeInsets.only(right: 12.0),
-            child: Icon(Icons.send),
-          )
-        ],
-    ),
+        actions:[
+          // action button
+          IconButton(
+          icon: Icon( Icons.send ),
+        onPressed: () {
+            Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => direct() ),); },
+      ),
+      ],
+          ),
+
+
+
       body: Container(
         margin: const EdgeInsets.all(16.0),
         child: Column(
@@ -98,26 +111,28 @@ class Home extends StatelessWidget {
           children: [
             storyProfile,
             Padding(
-              padding: const EdgeInsets.fromLTRB(16.0,16.0,8.0, 16.0),
+              padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Row(
                     children: [
                       Container(
-                        height: 40.0,width: 40.0,
+                        height: 40.0, width: 40.0,
                         decoration: const BoxDecoration(
                           shape: BoxShape.circle,
                           image: DecorationImage(
                               fit: BoxFit.fill,
-                              image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1200px-Circle-icons-profile.svg.jpg')
+                              image: NetworkImage(
+                                  'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1200px-Circle-icons-profile.svg.jpg')
                           ),
                         ),
                       ),
                       const SizedBox(
                         width: 10.0,
                       ),
-                      Text("Temporary",style: const TextStyle(fontWeight: FontWeight.bold,color:Colors.white),),
+                      Text("Temporary", style: const TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.white),),
                     ],
                   ),
                   const IconButton(
@@ -130,5 +145,5 @@ class Home extends StatelessWidget {
           ],
         ),
       ),);
-
   }
+}
