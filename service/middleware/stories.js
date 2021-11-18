64137bae9dc541e246dfe0342f67db031f18dcfc
Bakytbek Kenzhebekov <70631730+Bakytbek03042@users.noreply.github.com>
2021-11-18 23:52:21 +0600

    Delete service directory

--- service/middleware/stories.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const jwt = require('jsonwebtoken')
-const Publication = require('../models/publications')
-
-
-module.exports = router
