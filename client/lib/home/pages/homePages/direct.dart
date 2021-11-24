086716eedc62d2f7f0022aa505fd0abe2c5595c7
mahambetberikov <maxambet.010602@gmail.com>
2021-11-25 00:52:03 +0600

    modified interaction with direct page and home page + layout for direct initial

--- client/lib/home/pages/homePages/direct.dart
+++ client/lib/home/pages/homePages/direct.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+//import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'dart:convert';
 
@@ -7,43 +11,60 @@ import 'package:http/http.dart' as http;
 
 import 'home.dart';
 
+class Direct extends StatefulWidget {
+  @override
+  _DirectState createState() => _DirectState();
+}
 
-  class direct extends Home {
-    String name="rakhat02";
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        home:Scaffold(
-        appBar: AppBar(
+Future fetch() async {
+  SharedPreferences sharedPrefs = await SharedPreferences.getInstance();
 
-          title:Text(name,),
-          backgroundColor: Colors.black,
-            actions: [
-        // action button
-        IconButton(
-        icon: Icon( Icons.menu_open),
-          onPressed: () { },
+  final response = await http.get(
+    Uri.parse(
+        'http://localhost:5000/get/publications?token=${sharedPrefs.getString(
+            'token')}'),
+  );
+  print(response.body);
+}
+
+class _DirectState extends State<Direct> {
+  @override
+  Widget build(BuildContext context) {
+    fetch();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "login",
+          style: TextStyle(fontSize: 24.0, fontFamily: 'Arial'),
         ),
-              IconButton(
-                icon: Icon( Icons.edit),
-                onPressed: () { },
-              ),
-          ],
-          
-
-        ),
-          body: Container(
-            child:
-              const TextField(
-
-
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Search',
-                ),
-              )
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: Icon(
+              Icons.videocam,
+              size: 26.0,),
           ),
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: Icon(
+              Icons.message,
+              size: 26.0,),
+          ),
+        ],
       ),
-      );
-    }
+      body: SingleChildScrollView(child: Column(
+        children:[ Row(
+          children: [
+            Text("Все",style: TextStyle(color: Colors.white),),
+            Text("Звонки",style: TextStyle(color: Colors.white),),
+            Text("Запросы",style: TextStyle(color: Colors.white),),
+          ],
+        ),
+      ],
+    )
+    ),
+
+    );
   }
+}
--- client/lib/home/pages/homePages/home.dart
+++ client/lib/home/pages/homePages/home.dart
@@ -1,11 +1,14 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unnecessary_new
 
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import 'direct.dart';
+
 //import 'package:instagram/home/pages/profile.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -15,41 +18,54 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Future fetch() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    print(sharedPreferences.getString('token'));
+    final responce = await http.get(Uri.parse(
+        'http://localhost:5000/get/publications?token=${sharedPreferences.getString('token')}'));
+//    print(responce.body);
+  }
+
   final storyProfile = Expanded(
     child: ListView.builder(
       scrollDirection: Axis.horizontal,
-      itemBuilder: (context, index) =>
-          Stack(
-            alignment: Alignment.topLeft,
-            children: [
-              Container(
-                width: 60.0,
-                height: 60.0,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(image: NetworkImage(
-                      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1200px-Circle-icons-profile.svg.jpg'),
-                    fit: BoxFit.fill,
-                  ),
-
-                ),
-                margin: const EdgeInsets.symmetric(horizontal: 8.0),
+      itemBuilder: (context, index) => Stack(
+        alignment: Alignment.topLeft,
+        children: [
+          Container(
+            width: 60.0,
+            height: 60.0,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1200px-Circle-icons-profile.svg.jpg'),
+                fit: BoxFit.fill,
               ),
-              index == 0 ? const Positioned(
-                right: 10.0,
-                child: CircleAvatar(
-                  backgroundColor: Colors.blueAccent,
-                  radius: 10.0,
-                  child: Icon(Icons.add, size: 14.0,
-                    color: Colors.white,),
-                ),
-              ) : Container()
-            ],
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 8.0),
           ),
+          index == 0
+              ? const Positioned(
+                  right: 10.0,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.blueAccent,
+                    radius: 10.0,
+                    child: Icon(
+                      Icons.add,
+                      size: 14.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              : Container()
+        ],
+      ),
     ),
   );
 
   Widget build(BuildContext context) {
+    fetch();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
@@ -57,15 +73,16 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         leading: Icon(Icons.camera_alt),
         title: const Text("Instagram"),
-        actions:[
+        actions: [
           // action button
           IconButton(
-            icon: Icon( Icons.send ),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => direct() ),); },
-          ),
+              icon: Icon(Icons.send),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    PageTransition(
+                        type: PageTransitionType.rightToLeft, child: Direct()));
+              })
         ],
       ),
       body: Container(
@@ -76,10 +93,9 @@ class _HomeState extends State<Home> {
           mainAxisSize: MainAxisSize.min,
           children: [
             storyProfile,
-                ],
-              ),
-            ),
-
-        );
+          ],
+        ),
+      ),
+    );
   }
 }
